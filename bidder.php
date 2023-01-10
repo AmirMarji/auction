ba32a54f31fc5730d4070c72de1b16461d145a87
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2023-01-09 19:59:17 -0500

    added some functions to bidder class

--- bidder.php
+++ bidder.php
@@ -27,8 +27,38 @@ class Bidder {
         
     }
 
+    //save bidder to database
     function saveBidder(){
-        
+        //refactor later to be a include, just following book for now
+        $db = new mysqli("localhost","ah_user","AuctionHelper", "auction"); //DB Conection
+        $query = "INSERT INTO bidders VALUES(?,?,?,?,?)"; // SQL
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("issss", $this->bidderid, $this->lastname, $this->firstname, $this->address, $this->phone);
+        $result = $stmt->execute();
+        $db->close();
+        return $result;
     }
+    // update existing bidder based on a bidderid
+    function updateBidder(){
+        $db = new mysqli("localhost","ah_user","AuctionHelper", "auction"); //DB Conection
+        $query = "UPDATE bidders SET bidderid = ?, lastname = ?, firstname = ?, address = ?, phone = ? WHERE bidderid = $this->bidderid"; // SQL
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("issss", $this->bidderid, $this->lastname, $this->firstname, $this->address, $this->phone);
+        $result = $stmt->execute();
+        $db->close();
+        return $result;
+
+    }
+
+    function removeBidder(){
+        $db = new mysqli("localhost","ah_user","AuctionHelper", "auction"); //DB Conection
+        $query = "DELETE FROM bidders WHERE bidderid = $this->bidderid"; // SQL
+        $result = $db->query($query);
+        $db->close();
+        return $result;
+
+    }
+
+    
 
 }
